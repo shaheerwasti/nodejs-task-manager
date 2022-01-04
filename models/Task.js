d3508e5e20a84dc5b41674c51d749b97f7d18fdd
Shaheer <sm_shaheer@msn.com>
2022-01-04 19:57:01 +0500

    complete projectD

---
+++ models/Task.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const TaskSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"must provide name"],
+        trim:true,
+        maxlength:[20,'Name cannot be more than 20 char'],
+    },
+    completed:{
+        type:Boolean,
+        default:false,
+    }
+})
+
+module.exports = mongoose.model('Task',TaskSchema)
